c978972ffaa29990288c42c9824d0e95fb972bf9
Izolabella Geddis <iza.yy10@gmail.com>
2023-09-22 23:23:27 -0700

    🍥 quality updates + compatibility updates

--- Platforms/Tizen/Main.cs
+++ Platforms/Tizen/Main.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace izolabella.LoFi
 {
-    internal class Program : MauiApplication
+    internal sealed class  Program : MauiApplication
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 
